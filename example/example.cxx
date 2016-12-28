b9a9cbdecd689b05f9f851b8e4e36b5c6b8ae7b4
AkihiroTakai <at.sisy@gmail.com>
2016-12-28 14:03:43 +0900

    exampleファイルを編集

--- example/example.cxx
+++ example/example.cxx
@@ -11,12 +11,20 @@ void mouse(int x, int y){
       std::cout << "YURIGL" << std::endl;
 }
 
+void key(unsigned char key, int x, int y){
+      static int i;
+      yuri::FillBackGround(yuri::RGBA(0, 0, 0, 0));
+      yuri::DrawRect(yuri::Point2DD(i, 0), yuri::Point2DD(i+50, 0), yuri::Point2DD(i+50, 50), yuri::Point2DD(i, 50), yuri::RGBA(255, 255, 255, 255));
+      i++;
+}
+
 int main(int argc, char *argv[]){
       yuri::DisplaySetting(500, 500, 100, 100);
       yuri::Init(argc, argv);
       yuri::CreateWindow("window");
       yuri::SetRedrawFunction(display);
       yuri::SetDownLeftFunction(mouse);
+      yuri::SetKeyboardFunction(key);
       yuri::MainLoop();
       return 0;
 }
